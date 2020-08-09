19ea1c15a8bf141f3f1e56e9b52b204265707da1
SnusnumrConceit <snusnumr1996@gmail.com@>
2020-08-09 19:16:33 +0400

    Refactoring: AppointmentService logic has been moved to AppointmentController and then refacored by SRP. Only API methods has been saved.

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Appointment;
+use App\Exports\AppointmentExport;
+
+use App\Http\Requests\Admin\Appointment\IndexAppointment;
+use App\Http\Requests\Admin\Appointment\StoreAppointment;
+use App\Http\Requests\Admin\Appointment\UpdateAppointment;
+use App\Http\Requests\Admin\Appointment\DeleteAppointment;
+
+use App\Http\Resources\Admin\Appointment\DetailAppointment;
+use App\Http\Resources\Admin\Appointment\AppointmentCollection;
+
+class AppointmentController extends Controller
+{
+	public function __construct()
+	{
+		$this->authorizeResource('App\Appointment');
+	}
+	
+	/**
+	 * Display a listing of the appointments
+	 *
+	 * @param IndexAppointment $request
+	 *
+	 * @return AppointmentCollection
+	 *
+	 * @throws \Illuminate\Auth\Access\AuthorizationException
+	 */
+	public function index(IndexAppointment $request)
+	{
+		$this->authorize('index', Appointment::class);
+		
+		$appointments = Appointment::query();
+		
+		$appointments->when($request->keyword, function ($q, $keyword) {
+			return $q->where('name', 'LIKE', "%{$keyword}%");
+		});
+		
+		$appointments->when($request->filter, function ($q, $filter) {
+			$filter = json_decode($filter);
+			
+			return $q->orderBy($filter->name, $filter->type);
+		});
+		
+		$appointments = $appointments->paginate();
+		
+		return new AppointmentCollection($appointments);
+	}
+	
+	/**
+	 * Store a newly created appointment in storage
+	 *
+	 * @param StoreAppointment $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function store(StoreAppointment $request)
+	{
+		Appointment::create($request->validated());
+	
+		return response()->json([
+			'message' => __('appointments.created')
+		], 200);
+	}
+	
+	/**
+	 * Display the appointment resource.
+	 *
+	 * @param Appointment $appointment
+	 * @return DetailAppointment
+	 */
+	public function show(Appointment $appointment)
+	{
+		return new DetailAppointment($appointment);
+	}
+
+	/**
+	 * Update the appointment resource in storage.
+	 *
+	 * @param  UpdateAppointment  $request
+	 * @param  Appointment  $appointment
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(UpdateAppointment $request, Appointment $appointment)
+	{
+		$appointment->update($request->validated());
+	
+		return response()->json([
+			'message' => __('appointments.updated')
+		], 200);
+	}
+	
+	/**
+	 * Remove the appointment from storage.
+	 *
+	 * @param DeleteAppointment $request
+	 * @param Appointment $appointment
+	 * @return \Illuminate\Http\JsonResponse
+	 * @throws \Exception
+	 */
+	public function destroy(DeleteAppointment $request, Appointment $appointment)
+	{
+		$appointment->delete();
+		
+		return response()->json([
+			'message' => __('appointments.deleted')
+		], 200);
+	}
+	
+	/**
+	 * Export appointments in excel format
+	 *
+	 * @return AppointmentExport
+	 */
+	public function export()
+	{
+		return new AppointmentExport();
+	}
+}
