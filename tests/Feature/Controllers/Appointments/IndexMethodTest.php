713b1ac7084df991573905269052e171af3357b4
SnusnumrConceit <snusnumr1996@gmail.com@>
2020-08-12 01:17:12 +0400

    Testing: add permission as param of creating acting as user Appointments/IndexMethodTest.

--- tests/Feature/Controllers/Appointments/IndexMethodTest.php
+++ tests/Feature/Controllers/Appointments/IndexMethodTest.php
@@ -9,43 +9,43 @@ use App\Http\Requests\Admin\Appointment\IndexAppointment;
 
 class IndexMethodTest extends BaseControllerTest
 {
-	protected function setUp(): void
-	{
-		parent::setUp();
-		
-		$this->controllerName = AppointmentController::class;
-		$this->formRequestName = IndexAppointment::class;
-		$this->actionName = 'index';
-	}
-	
-	/**
-	 * Test controller has method
-	 */
-	public function testIndexActionExists()
-	{
-		$controller = new $this->controllerName;
-		
-		$this->assertTrue(method_exists($controller, $this->actionName));
-	}
-	
-	/**
-	 * A basic feature test example.
-	 *
-	 * @return void
-	 */
-	public function testIndexAction()
-	{
-		$this->checkControllerHasFormRequest();
-	
-		$this->be($this->createAndGetUser());
-	
-		$request = $this->getFormRequestInstance();
-		$response = (new $this->controllerName)->{$this->actionName}($request);
-		
-		$this->assertInstanceOf(JsonResponse::class, $response);
-		$this->assertTrue($response->isOk());
-		$this->assertEquals('application/json', $response->headers->get('Content-Type'));
-		
-		$this->assertTrue(property_exists($response->getData(), 'appointments'));
-	}
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->controllerName = AppointmentController::class;
+        $this->formRequestName = IndexAppointment::class;
+        $this->actionName = 'index';
+    }
+    
+    /**
+     * Test controller has method
+     */
+    public function testIndexActionExists()
+    {
+        $controller = new $this->controllerName;
+        
+        $this->assertTrue(method_exists($controller, $this->actionName));
+    }
+    
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndexAction()
+    {
+        $this->checkControllerHasFormRequest();
+        
+        $this->be($this->createAndGetUser(['appointments_view']));
+    
+        $request = $this->getFormRequestInstance();
+        $response = (new $this->controllerName)->{$this->actionName}($request);
+        
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertTrue($response->isOk());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        
+        $this->assertTrue(property_exists($response->getData(), 'appointments'));
+    }
 }
