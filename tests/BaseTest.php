c75eec539640a86fd0ed9ae92face12da9632704
SnusnumrConceit <snusnumr1996@gmail.com@>
2020-08-12 01:12:56 +0400

    Testing: permission assigning has been added to creating user test method.

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,26 +2,35 @@
 
 namespace Tests;
 
+use App\User;
 use JMac\Testing\Traits\AdditionalAssertions;
 
 class BaseTest extends TestCase
 {
-	use AdditionalAssertions;
-	
-	protected $data = [];
-	
-	protected function setUp(): void
-	{
-		parent::setUp();
-	}
-	
-	/**
-	 * Create and get created user
-	 *
-	 * @return mixed
-	 */
-	protected function createAndGetUser()
-	{
-		return factory('App\User')->create();
-	}
+    use AdditionalAssertions;
+    
+    protected $data = [];
+    
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+    
+    /**
+     * Create and get created user
+     *
+     * @param array $permissions
+     *
+     * @return User
+     */
+    protected function createAndGetUser(array $permissions = [])
+    {
+        $user = factory(User::class)->create();
+        
+        if ($permissions) {
+            $user->permissions()->sync($permissions);
+        }
+        
+        return $user;
+    }
 }
